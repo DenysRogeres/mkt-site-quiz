a92724f37453fa384f218e48080cd944a41572be
DenysRogeres <dennys.rogeres@gmail.com>
2024-05-01 22:26:23 -0300

    corrigindo importacao

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Terms } from './pages/Terms';
 import { Balmorex } from "./pages/Balmorex";
 import { Vsl } from "./pages/VSL/Vsl";
 import { Home } from './pages/Home';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Analytics } from "@vercel/analytics/react"
 
 
