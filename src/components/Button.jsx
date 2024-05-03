46c497b97df12c6058fcb209fd87e4423586aaa6
DenysRogeres <dennys.rogeres@gmail.com>
2024-05-03 02:18:32 -0300

    adicionando trackamento

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import './Button.css';
 
 export function Button() {
    return (
-      <a id="ps-click-vsl" href="https://hop.clickbank.net/?affiliate=denysroger&vendor=balmorex&pid=v4&tid=myhealthtoday">
+      <a id="ps-click-vsl" href="https://hop.clickbank.net/?affiliate=denysroger&vendor=balmorex&pid=v4&tid=[cmc_vid]">
          <button id="ps-click-vsl" className="myButton">
             CLICK HERE NOW TO WATCH
          </button>
--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -7,7 +7,7 @@ export function VideoSection({ vagas }) {
          <h1 className={styles.title}>A Once <span className={styles.mark}>Banned Back Pain</span> Fix From The 60’S Stuns Doctors</h1>
          
          <p className={styles.subtitle}>There are <strong className={styles.mark}>{vagas}</strong> places left for this <strong>FREE</strong> Presentation</p>
-         <a href="https://hop.clickbank.net/?affiliate=denysroger&vendor=balmorex&pid=v4&tid=myhealthtoday" id="ps-click-vsl">
+         <a href="https://hop.clickbank.net/?affiliate=denysroger&vendor=balmorex&pid=v4&tid=[cmc_vid]" id="ps-click-vsl">
             <img id="ps-click-vsl" src="https://res.cloudinary.com/dvmbmpthe/image/upload/v1714702437/gif-balmorex-aprimorado_fjyit3.gif" alt="Video" className={styles.video}/>
          </a>
       </div>
