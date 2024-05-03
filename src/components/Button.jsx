7dc777e4689164907b84e91e7ce5e04bc00b7e12
DenysRogeres <dennys.rogeres@gmail.com>
2024-05-03 02:10:15 -0300

    trackeamento para action

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,8 @@ import './Button.css';
 
 export function Button() {
    return (
-      <a href="https://hop.clickbank.net/?affiliate=denysroger&vendor=balmorex&pid=v4&tid=myhealthtoday">
-         <button className="myButton">
+      <a id="ps-click-vsl" href="https://hop.clickbank.net/?affiliate=denysroger&vendor=balmorex&pid=v4&tid=myhealthtoday">
+         <button id="ps-click-vsl" className="myButton">
             CLICK HERE NOW TO WATCH
          </button>
       </a>
